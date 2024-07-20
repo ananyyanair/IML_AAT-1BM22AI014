fb4ebff852bacaff00f24e69d9ddda2125caa252
ananyyanair <ananya.ai22@bmsce.ac.in>
2024-07-20 17:35:22 +0530

    Update basic_probability_problem#6.py

--- basic_probability_problem#6.py
+++ basic_probability_problem#6.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 x = ['w']*5 + ['b']*4
 y = ['w']*7 + ['b']*6
-def black_probability(x, y):
+def blackprob(x, y):
     return (Fraction(x.count('w') * y.count('b'), len(x) * (len(y)+1)) + Fraction(x.count('b') * (y.count('b')+1), len(x) * (len(y)+1)))  
-print(black_probability(x, y))
+print(blackprob(x, y))
